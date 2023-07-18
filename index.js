f4e2244787e3220671deb3f2ea26b0d45a5030fb
yogita0923 <yp109823@gmail.com>
2023-07-18 17:18:00 +0530

    hello all of you

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var express = require(‘express’);
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get(‘/’, function (req, res) {
-res.send(‘hello all’);
+res.send(‘hello all of you’);
 });
 //listen to port 80 by default
 app.listen(process.env.PORT || 80);
